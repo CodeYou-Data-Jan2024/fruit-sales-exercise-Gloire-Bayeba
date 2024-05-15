1c23a5344736297014484326cc02a6f7a6c09f40
gloire-bayeba <gloirebayeba@gmail.com>
2024-05-15 17:00:45 -0400

    changes made

--- fruitsales.py
+++ fruitsales.py
@@ -1,4 +1,5 @@
 import pandas as pd
-pd.DataFrame({"Apples":[35,41], 
+fruit_sales= pd.DataFrame({"Apples":[35,41], 
              "Bananas":[21,34]},
             index=["2017 Sales", "2018 Sales"])# add your code here
+fruit_sales.to_csv("fruit.csv")
